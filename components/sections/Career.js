f2cd378603a88e6864efd7faa6ee9fe9cffcb888
Brissou <brice.eliasse@gmail.com>
2025-03-29 15:33:26 +0100

    added selego

--- components/sections/Career.js
+++ components/sections/Career.js
@@ -1,39 +1,51 @@
-"use client"
+"use client";
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
 const jobs = [
+  {
+    company: "Selego",
+    posts: [
+      {
+        name: "VIE - Développeur Full-Stack",
+        startDate: "Janv. 2025",
+        endDate: "Aujourd'hui",
+      },
+    ],
+    slug: "selego",
+    pics: 3,
+  },
   {
     company: "La Capsule",
     posts: [
       {
         name: "Principal Teacher",
         startDate: "Janv. 2024",
-        endDate: "Aujourd'hui",
+        endDate: "Janv. 2025",
       },
       {
         name: "Teacher",
         startDate: "Mars 2023",
         endDate: "Janv. 2024",
-      }
+      },
     ],
     slug: "capsule",
-    pics: 3
+    pics: 3,
   },
   {
     company: "MediaSchool",
     posts: [
       {
         name: "Intervenant professionnel",
-        startDate: "Janv. 2022",
-        endDate: "Aujourd'hui",
-      }
+        startDate: "Sept. 2023",
+        endDate: "Août 2024",
+      },
     ],
     slug: "mediaschool",
-    pics: 2
-
+    pics: 2,
   },
-  , {
+  ,
+  {
     company: "Nanaba",
     posts: [
       {
@@ -45,11 +57,10 @@ const jobs = [
         name: "Développeur Full-Stack",
         startDate: "Sept 2021",
         endDate: "Janv. 2022",
-      }
+      },
     ],
     slug: "nanaba",
-    pics: 3
-
+    pics: 3,
   },
   {
     company: "MWA",
@@ -61,7 +72,6 @@ const jobs = [
       },
     ],
     slug: "mwa",
-
   },
   {
     company: "Koedia",
@@ -85,9 +95,7 @@ const jobs = [
     ],
     slug: "visiplus",
   },
-]
-
-
+];
 
 export default function Career() {
   const [selectedJob, setSelectedJob] = useState(jobs[0]);
@@ -110,16 +118,14 @@ export default function Career() {
             width={400}
             height={300}
             alt="Photo de Brice Eliasse"
-            className="rounded-2xl w-full h-96 object-cover" />
-
+            className="rounded-2xl w-full h-96 object-cover"
+          />
         </AnimatePresence>
 
         <div className="flex justify-between gap-5 mt-4 h-56 w-full">
-          <AnimatePresence mode="wait" >
-
-            {
-              !!selectedJob.pics &&
-              [...Array(selectedJob.pics)].map((el, i) =>
+          <AnimatePresence mode="wait">
+            {!!selectedJob.pics &&
+              [...Array(selectedJob.pics)].map((el, i) => (
                 <motion.img
                   src={`/companies/photos/${selectedJob.slug}_${i + 1}.webp`}
                   key={selectedJob.slug + i}
@@ -130,45 +136,71 @@ export default function Career() {
                     type: "spring",
                     stiffness: 260,
                     damping: 20,
-                    delay: 0.15 * i
-                  }} height={224} alt={`Photo de ${el}`} className={`rounded-xl object-cover min-w-0 ${selectedJob.pics === 1 ? "w-full" : selectedJob.pics === 2 ? "w-1/2" : "w-1/3"}`} />
-              )}
+                    delay: 0.15 * i,
+                  }}
+                  height={224}
+                  alt={`Photo de ${el}`}
+                  className={`rounded-xl object-cover min-w-0 ${
+                    selectedJob.pics === 1
+                      ? "w-full"
+                      : selectedJob.pics === 2
+                      ? "w-1/2"
+                      : "w-1/3"
+                  }`}
+                />
+              ))}
           </AnimatePresence>
         </div>
-
       </div>
       <div className="col-span-12 md:col-span-6 flex flex-col gap-6 ">
-        <h2>C'était un réel plaisir de <br /><span className="text-primary">travailler ensemble</span></h2>
+        <h2>
+          C'était un réel plaisir de <br />
+          <span className="text-primary">travailler ensemble</span>
+        </h2>
         <p>Merci à toutes les entreprises qui m'ont fait confiance ↓</p>
 
         <div className="border-2 border-gray-700 rounded-xl overflow-hidden">
-          {jobs.map((job, i) =>
+          {jobs.map((job, i) => (
             <div
               key={job.slug}
-              className={`px-4 py-2.5  ${job.slug === selectedJob.slug ? "bg-gray-800" : i <= 3 ? "transition cursor-pointer hover:bg-gray-800 " : ""}`}
-              onClick={() => i <= 3 ? setSelectedJob(job) : false}
+              className={`px-4 py-2.5  ${
+                job.slug === selectedJob.slug
+                  ? "bg-gray-800"
+                  : i <= 4
+                  ? "transition cursor-pointer hover:bg-gray-800 "
+                  : ""
+              }`}
+              onClick={() => (i <= 4 ? setSelectedJob(job) : false)}
             >
               <div className="flex items-start gap-2">
-                <img src={`/companies/${job.slug}.png`} width={40} height={40} alt={`Logo ${job.company}`} />
+                <img
+                  src={`/companies/${job.slug}.png`}
+                  width={40}
+                  height={40}
+                  alt={`Logo ${job.company}`}
+                  className="rounded"
+                />
                 <div className="grow">
-                  <h3 className=" font-semibold tracking-wide mb-1 text-sm">{job.company}</h3>
-                  {job.posts.map((post, i) =>
-                    <div key={i} className="flex justify-between items-center text-xs text-base-content opacity-75 mb-0.5">
+                  <h3 className=" font-semibold tracking-wide mb-1 text-sm">
+                    {job.company}
+                  </h3>
+                  {job.posts.map((post, i) => (
+                    <div
+                      key={i}
+                      className="flex justify-between items-center text-xs text-base-content opacity-75 mb-0.5"
+                    >
                       <h4>{post.name}</h4>
                       <div>
                         {post.startDate} - {post.endDate}
                       </div>
                     </div>
-                  )}
+                  ))}
                 </div>
               </div>
-
-
-            </div>)}
+            </div>
+          ))}
         </div>
-
       </div>
     </div>
-
-  )
+  );
 }
