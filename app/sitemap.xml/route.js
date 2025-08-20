35054a06c866772b3cc0fb09ce512970b2f93526
Brissou <brice.eliasse@gmail.com>
2025-08-20 21:06:21 +0200

    good

--- app/sitemap.xml/route.js
+++ app/sitemap.xml/route.js
@@ -4,7 +4,7 @@ const BASE_URL = "https://brice-eliasse.com";
 // Tokens par langue (ton CMS headless)
 const TOKENS = {
   fr: "203377ab-1537-4b08-a5ec-93d090abc95e",
-  en: "35e6fafc-82b3-421a-9e74-57d21a92450c",
+  en: "2f8dd773-4ba0-4b3c-99ab-bb0a92a130b2",
 };
 
 // selon ta pagination (9 par page dans ton code)
